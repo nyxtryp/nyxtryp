cd8041934fe7ab9c8b7296e143dba752e22b5010
nyxtryp <nyxtryp@gmail.com>
2026-08-12 17:38:05 +0800

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <main className="nyxtryp">
+      <section className="intro">
+        <p className="artist">NYXTRYP</p>
+
+        <h1>DON'T LISTEN. ENTER.</h1>
+
+        <button className="enter-button">
+          ENTER
+        </button>
+      </section>
+    </main>
+  )
+}
