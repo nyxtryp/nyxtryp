426d00a202611d4fca9e36f27219bcc365e273c6
nyxtryp <nyxtryp@gmail.com>
2026-09-05 00:37:55 +0800

    feat: add private admin route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
 import Scene from './scene/Scene.jsx'
+import AdminPanel from './components/AdminPanel.jsx'
 
 export default function App() {
+  if (window.location.pathname === '/admin') {
+    return <AdminPanel />
+  }
+
   return (
     <main className="nyxtryp">
       <Scene />
